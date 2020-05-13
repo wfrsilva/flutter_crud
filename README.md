7901c0f4bceed395811ac82b4b6257fb80ef6e4c
Wendel Silva <wfrsilva@gmail.com>
2020-05-13 19:21:00 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_crud
+ Cadastro COMPLETO em Flutter! CRUD... Create, Read, Update e Delete.
---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,21 @@
+const DUMMY_USERS = {
+  '1' : const User(
+    id: '1',
+    name: 'Maria',
+    email: 'maria@alunos.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2020/04/15/17/32/medical-5047582_960_720.png'
+  ),
+  '2' : const User(
+    id: '2',
+    name: 'Rafael',
+    email: 'rafael@alunos.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331_960_720.png'
+  ),
+  '3' : const User(
+    id: '3',
+    name: 'Alice',
+    email: 'alice@alunos.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2014/03/24/17/19/teacher-295387_960_720.png'
+  ),
+
+};
