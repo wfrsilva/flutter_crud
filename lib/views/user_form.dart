bb28898f2c6bd531de06b0739d1cda674857bc4b
Wendel Silva <wfrsilva@gmail.com>
2020-05-14 18:39:53 -0300

    01:04:00 - Salvando usuario
    01:04:00 - Salvando usuario
    https://youtu.be/ViahqKZzZ7Y?t=3879
    Cadastro COMPLETO em Flutter! CRUD... Create, Read, Update e Delete.

--- lib/views/user_form.dart
+++ lib/views/user_form.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_crud/models/user.dart';
+import 'package:flutter_crud/provider/users.dart';
+import 'package:provider/provider.dart';
 
 class UserForm extends StatelessWidget {
 
   final _form = GlobalKey<FormState>();
+  final Map<String, String> _formData = {};
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +20,16 @@ class UserForm extends StatelessWidget {
               final isValid = _form.currentState.validate();
               if(isValid){
                 _form.currentState.save();
+
+                Provider.of<Users>(context, listen: false).put(
+                  User(
+                  id: _formData['id'],
+                  name: _formData['name'],
+                  email: _formData['email'],
+                  avatarUrl: _formData['avatarUrl'],
+                  ),
+                );
+
                 Navigator.of(context).pop();  
               }//if
             },//onPressed
@@ -41,16 +55,16 @@ class UserForm extends StatelessWidget {
                   }//if
                 },//validator
 
-                onSaved: (value){
-                  print(value);
-                },//onSaved
+                onSaved: (value) => _formData['name'] = value,
 
               ),
               TextFormField(
                 decoration: InputDecoration(labelText: 'E-mail'),
+                onSaved: (value) => _formData['email'] = value,
               ),
               TextFormField(
                 decoration: InputDecoration(labelText: 'URL do Avatar'),
+                onSaved: (value) => _formData['avatarUrl'] = value,
               ),
             ],
           )
