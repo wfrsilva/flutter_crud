d62ad66e2e4cf625d3b8ce41aa90a74d41775fee
Wendel Silva <wfrsilva@gmail.com>
2020-05-14 18:59:19 -0300

    01:08:00 - Botao alterar funcionando
    01:08:00 - Botao alterar funcionando
    botao chama formulario com as informacoes do usuario
    botao salva as alteracoes

--- lib/views/user_form.dart
+++ lib/views/user_form.dart
@@ -8,8 +8,19 @@ class UserForm extends StatelessWidget {
   final _form = GlobalKey<FormState>();
   final Map<String, String> _formData = {};
 
+  void _loadFormData (User user){
+    _formData['id'] = user.id;
+    _formData['name'] = user.name;
+    _formData['email'] = user.email;
+    _formData['avatarUrl'] = user.avatarUrl;
+  }//_loadFormData
+
   @override
   Widget build(BuildContext context) {
+    final User user = ModalRoute.of(context).settings.arguments;
+    
+    _loadFormData(user);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Formulário de Usuário'),
@@ -23,10 +34,10 @@ class UserForm extends StatelessWidget {
 
                 Provider.of<Users>(context, listen: false).put(
                   User(
-                  id: _formData['id'],
-                  name: _formData['name'],
-                  email: _formData['email'],
-                  avatarUrl: _formData['avatarUrl'],
+                    id: _formData['id'],
+                    name: _formData['name'],
+                    email: _formData['email'],
+                    avatarUrl: _formData['avatarUrl'],
                   ),
                 );
 
@@ -44,6 +55,7 @@ class UserForm extends StatelessWidget {
           child: Column(
             children: <Widget>[
               TextFormField(
+                initialValue: _formData['name'],
                 decoration: InputDecoration(labelText: 'Nome'),
                 validator: (value){
                   if(value == null|| value.trim().isEmpty){
@@ -59,10 +71,12 @@ class UserForm extends StatelessWidget {
 
               ),
               TextFormField(
+                initialValue: _formData['email'],
                 decoration: InputDecoration(labelText: 'E-mail'),
                 onSaved: (value) => _formData['email'] = value,
               ),
               TextFormField(
+                initialValue: _formData['avatarUrl'],
                 decoration: InputDecoration(labelText: 'URL do Avatar'),
                 onSaved: (value) => _formData['avatarUrl'] = value,
               ),
